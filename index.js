940395bfd355a380d07efb422671e43a196e33ef
Ramneekg <42669256+Ramneekg@users.noreply.github.com>
2019-12-15 14:26:01 -0800

    CORS

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const app = express();
 const winston = require("winston");
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 require("./startup/logging")();
-app.use(cors());
+// app.use(cors());
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
