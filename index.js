136307abf1a939143b2768ed09933d290703e166
Ramneekg <42669256+Ramneekg@users.noreply.github.com>
2019-12-15 14:33:03 -0800

    addedcors

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const express = require("express");
 // const cors = require("cors");
 const app = express();
 const winston = require("winston");
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "POST, PUT, GET, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
   next();
 });
 require("./startup/logging")();
