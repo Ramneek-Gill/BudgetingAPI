181646af4b628d0de569d2eb5546ea1eee93f63d
Ramneekg <42669256+Ramneekg@users.noreply.github.com>
2019-12-15 16:09:41 -0800

    fixedcategory

--- routes/transactions.js
+++ routes/transactions.js
@@ -39,7 +39,7 @@ router.put("/:id", async (req, res) => {
     req.params.id,
     {
       item: req.body.item,
-      genre: {
+      category: {
         _id: category._id,
         name: category.name
       },
